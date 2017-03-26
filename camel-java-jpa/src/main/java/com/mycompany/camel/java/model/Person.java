c8da6d1802265dbd4346d7195e3c9c1d7c79d42b
Trifon <trifonnt@users.noreply.github.com>
2017-03-26 18:06:33 +0300

    Minor code formatting.

--- camel-java-jpa/src/main/java/com/mycompany/camel/java/model/Person.java
+++ camel-java-jpa/src/main/java/com/mycompany/camel/java/model/Person.java
@@ -13,29 +13,29 @@ import javax.xml.bind.annotation.XmlType;
 @Entity(name="Person")
 public class Person {
 	private Long id;
-private String name;
-private String surname;
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getSurname() {
-	return surname;
-}
-public void setSurname(String surname) {
-	this.surname = surname;
-}
-@Id
-@GeneratedValue(strategy=GenerationType.AUTO)
-public Long getId() {
-    return id;
-}
+	private String name;
+	private String surname;
 
-public void setId(Long id) {
-    this.id = id;
-}
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 }
